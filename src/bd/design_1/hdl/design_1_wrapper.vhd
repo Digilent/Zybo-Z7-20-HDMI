866ff368edfe911baaf516b39a5291571bcb484a
SamKLowe <samuel.lowe@ni.com>
2016-11-06 18:51:28 -0800

    Deleted sdk project

--- src/bd/design_1/hdl/design_1_wrapper.vhd
+++ src/bd/design_1/hdl/design_1_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2016.2 (win64) Build 1577090 Thu Jun  2 16:32:40 MDT 2016
---Date        : Sun Nov 06 18:24:47 2016
+--Date        : Sun Nov 06 18:39:05 2016
 --Host        : DESKTOP-C0MUAGJ running 64-bit major release  (build 9200)
 --Command     : generate_target design_1_wrapper.bd
 --Design      : design_1_wrapper
